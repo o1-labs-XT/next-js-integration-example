22f8751c90448f3e2cf1667c68d3179fde7f4820
Coby <coby@o1labs.org>
2025-01-27 17:58:49 -0500

    fix initialization error

--- src/components/PendingTodosQueue.tsx
+++ src/components/PendingTodosQueue.tsx
@@ -5,6 +5,11 @@ export default function TodosQueue({ title, subheading, todos }: { title: string
         <div className="flex flex-col gap-2 w-3/4">
             <h1 className="text-2xl font-bold">{title}</h1>
             <p>{subheading}</p>
+            {todos.length === 0 ? (
+                <p className="font-bold">No todos pending</p>
+            ) : (
+                <p className="font-bold">Pending:</p>
+            )}
             <ul className="flex flex-col gap-2">
                 {todos.map((todo, index) => (
                     <PendingTodoItem key={index} todo={todo} />
